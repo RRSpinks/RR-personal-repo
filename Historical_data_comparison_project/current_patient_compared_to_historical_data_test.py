b2dd9e1c0c957af639dbccaf045f19fb4cd473fe
Richard Spinks <rrspinks3@gmail.com>
2023-04-04 09:09:53 +0930

    improved historical data analysis

--- Historical_data_comparison_project/current_patient_compared_to_historical_data_test.py
+++ Historical_data_comparison_project/current_patient_compared_to_historical_data_test.py
@@ -15,40 +15,34 @@ output_dir = os.path.join(parent_dir, "Data", "Historical_01", "Output")
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-# Import data
-df = pd.read_csv(os.path.join(input_dir, "Abdomen CT parameters vs Patient age, sex - out.csv"))
+np.random.seed(42)
+# Custom function to generate feature size values based on age
+def generate_feature_size(age, peak_age=28):
+    scale_factor = peak_age / 3
+    shift_factor = age - peak_age
+    return np.random.gamma(age / scale_factor, scale_factor) + shift_factor
 
-#np.random.seed(42)
-## Custom function to generate feature size values based on age
-#def generate_feature_size(age, peak_age=28):
-#    scale_factor = peak_age / 3
-#    shift_factor = age - peak_age
-#    return np.random.gamma(age / scale_factor, scale_factor) + shift_factor
-
-## Create the randomized dataset
-#data = {
-#    "scan_type": np.random.choice(["CT", "MRI", "Ultrasound"], 5000),
-#    "patient_sex": np.random.choice(["M", "F"], 5000),
-#    "patient_age": np.random.randint(18, 100, 5000),
-#}
-#df = pd.DataFrame(data)
-#f["feature_size"] = df["patient_age"].apply(generate_feature_size)
-
-attr = "liver_volume"
+# Create the randomized dataset
+data = {
+    "scan_type": np.random.choice(["CT", "MRI", "Ultrasound"], 5000),
+    "patient_sex": np.random.choice(["M", "F"], 5000),
+    "patient_age": np.random.randint(18, 100, 5000),
+}
+df = pd.DataFrame(data)
+df["feature_size"] = df["patient_age"].apply(generate_feature_size)
 
 # Create the current patient's data
 current_patient = {
     "scan_type": "CT",
     "patient_sex": "M",
     "patient_age": 35,
-    attr: 60,
+    "feature_size": 60,
 }
 
 # Filter the data by patient_sex, patient_age, and scan_type
 filtered_data = df[
     (df["patient_sex"] == current_patient["patient_sex"]) &
     (df["patient_age"] >= 18)]
-filtered_data = filtered_data["patient_age", "patient_sex", attr]
 
 # Calculate percentiles for each age group
 percentiles = [0, 20, 40, 60, 80, 100]
@@ -56,7 +50,7 @@ colors = ['indianred', 'indianred', 'gold', 'limegreen', 'gold', 'indianred']
 unique_ages = np.sort(filtered_data["patient_age"].unique())
 age_percentiles = []
 for age in unique_ages:
-    age_data = filtered_data[filtered_data["patient_age"] == age][attr]
+    age_data = filtered_data[filtered_data["patient_age"] == age]["feature_size"]
     age_percentiles.append([age] + [np.percentile(age_data, p) for p in percentiles])
 age_percentiles_df = pd.DataFrame(age_percentiles, columns=["patient_age"] + percentiles)
 
@@ -84,13 +78,13 @@ for p in percentiles:
     cs_dict[p] = CubicSpline(age_percentiles_df["patient_age"], age_percentiles_df[p])
 
 # Plot the scatter plot dots
-sns.scatterplot(data=filtered_data, x="patient_age", y=attr, alpha=0.4, color='grey', edgecolors='none', linewidth=0)
-plt.scatter(current_patient["patient_age"], current_patient[attr], color='red')
+sns.scatterplot(data=filtered_data, x="patient_age", y="feature_size", alpha=0.4, color='grey', edgecolors='none', linewidth=0)
+plt.scatter(current_patient["patient_age"], current_patient["feature_size"], color='red')
 
 # Calculate the median trendline
-median_values = filtered_data.groupby("patient_age").median()[attr].reset_index()
+median_values = filtered_data.groupby("patient_age").median()["feature_size"].reset_index()
 window_size = 10
-median_values["smoothed_median"] = median_values[attr].rolling(window=window_size, center=True, min_periods=1).median()
+median_values["smoothed_median"] = median_values["feature_size"].rolling(window=window_size, center=True, min_periods=1).median()
 
 # Plot the median trendline
 plt.plot(median_values["patient_age"],
@@ -104,7 +98,7 @@ plt.plot(median_values["patient_age"],
 current_patient_percentiles = [cs_dict[x](current_patient["patient_age"]) for x in percentiles]
 outcome = None
 for idx, value in enumerate(current_patient_percentiles):
-    if current_patient[attr] <= value:
+    if current_patient["feature_size"] <= value:
         outcome = f"{percentiles[idx]}th Percentile"
         break
 if outcome is None:
@@ -123,8 +117,8 @@ f"Outcome: {outcome}"
 # Determine the box color for patient info based on the outcome percentile
 box_color = "white"
 for idx, value in enumerate(current_patient_percentiles):
-    if current_patient[attr] <= value:
-        box_color = colors[idx + 1]
+    if current_patient['feature_size'] <= value:
+        box_color = colors[idx]
         break
 
 # Display the patient information box below the legend
@@ -145,7 +139,7 @@ plt.text(
 x2 = x1+0.05
 y2 = y1-0.18
 arrow_start = (x2, y2)
-arrow_end = (current_patient["patient_age"], current_patient[attr])
+arrow_end = (current_patient["patient_age"], current_patient["feature_size"])
 plt.annotate("",
              xy=arrow_end,
              xycoords='data',
